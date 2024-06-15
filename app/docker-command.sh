e65cd8dc09add5e4589a0de36addceff846c264a
EC2 Default User <ec2-user@ip-172-31-25-94.ap-southeast-1.compute.internal>
2024-06-15 07:14:40 +0000

    FIX add --break-system-packages in installation

--- app/docker-command.sh
+++ app/docker-command.sh
@@ -2,13 +2,13 @@
 
 docker buildx build -t sqs-reader --platform=linux/amd64 -f keda/Dockerfile ./keda
 docker login
-docker tag sqs-reader:latest khanasif1/sqs-reader:v0.12
-docker push khanasif1/sqs-reader:v0.12
+docker tag sqs-reader:latest yiuc88/sqs-reader:v0.12
+docker push yiuc88/sqs-reader:v0.12
 
 
 #KARPENTER -Build the image
 
 docker buildx build -t karpenter-sqs-reader --platform=linux/amd64 -f karpenter/Dockerfile ./karpenter
 docker login
-docker tag karpenter-sqs-reader:latest khanasif1/karpenter-sqs-reader:v0.2
-docker push khanasif1/karpenter-sqs-reader:v0.2
+docker tag karpenter-sqs-reader:latest yiuc88/karpenter-sqs-reader:v0.2
+docker push yiuc88/karpenter-sqs-reader:v0.2
